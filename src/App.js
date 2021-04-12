6199954cbe99c546c6454fb7098b7d2920128423
Steven <Mahalak.steve@outlook.com>
2021-04-12 19:29:36 -0400

    check uncheck working

--- src/App.js
+++ src/App.js
@@ -9,11 +9,38 @@ class App extends React.Component {
     this.state = {
       todos: todosData
     }
+    // this binds handleChange() needed because of setState()
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
 
+  handleChange(id) {
+    this.setState((prevState) => {
+      // using map because it will provide us with a new array
+      const updatedTodo = prevState.todos.map((todo) => {
+        if (todo.id === id) {   // looks at the item with the same id that the handleChange() received from the onChange handler
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+          // returns a brand new object that will replace that todo in the array
+          // ... says give me all of the properties from this todo but override the completed property
+
+        }
+        return todo
+      })
+      // here the new array replaces the old array and that is what is rendered
+      return {
+        todos: updatedTodo
+      }
+    })
+  }
+
+
+  render() {
+    //  information from the item object is passed down to TodoItem. item prop is passed with a value of the item object
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+    // handleChange() is being passed down to every single toDo item that we create in our map here
     return (
       <div className="todo-list">
         {todoItems}
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,11 +2,17 @@ import React from "react"
 
 function TodoItem(props) {
     return (
+        // anytime a checkbox is checked it calls onChange which is receiving the event object; which calls handleChange() and passes this item's id
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed} />
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={(event) => props.handleChange(props.item.id)} />
+
             <p>{props.item.text}</p>
         </div>
     )
 }
 
 export default TodoItem
+
